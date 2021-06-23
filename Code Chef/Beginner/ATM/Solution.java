6c817eeec52ce11237280ebeca5c73f2c94f4f60
Ela Singh <ela4490@gmail.com>
2021-06-23 22:18:13 +0200

    Added Code Chef Solution

---
+++ Code Chef/Beginner/ATM/Solution.java
@@ -0,0 +1,57 @@
+/* Pooja would like to withdraw X $US from an ATM. The cash machine will only accept the transaction if X is a multiple of 5, and Pooja's account balance has enough cash to perform the withdrawal transaction (including bank charges). For each successful withdrawal the bank charges 0.50 $US. Calculate Pooja's account balance after an attempted transaction.
+
+Input
+Positive integer 0 < X <= 2000 - the amount of cash which Pooja wishes to withdraw.
+Nonnegative number 0<= Y <= 2000 with two digits of precision - Pooja's initial account balance.
+
+Output
+Output the account balance after the attempted transaction, given as a number with two digits of precision. If there is not enough money in the account to complete the transaction, output the current bank balance.
+
+Example - Successful Transaction
+Input:
+30 120.00
+Output:
+89.50
+
+Example - Incorrect Withdrawal Amount (not multiple of 5)
+Input:
+42 120.00
+Output:
+120.00
+
+Example - Insufficient Funds
+Input:
+300 120.00
+Output:
+120.00
+
+Link: https://www.codechef.com/problems/HS08TEST */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		try{
+		    Scanner sc = new Scanner(System.in);
+		    double withdraw_amount = sc.nextDouble();
+		    double initial_amount = sc.nextDouble();
+		    
+		    if(withdraw_amount%5==0 && initial_amount>=withdraw_amount+0.5){
+		        double balance = initial_amount-(withdraw_amount+0.5);
+		        System.out.println(balance);
+		    }else{
+		        System.out.println(initial_amount);
+		    }
+		}catch(Exception e){
+		    return;
+		}
+	}
+}
