3c99e685e96caad6c1268253d233d2f60a7ede71
Ela Singh <ela4490@gmail.com>
2021-06-29 13:17:41 +0200

    Create Max Consecutive Ones III

---
+++ Leet Code/Coding Challenges/June LeetCode Challenge 2021/Week 5 June 29th - June 30th/Max Consecutive Ones III/Solution.java
@@ -0,0 +1,40 @@
+/* Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+Example 1:
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+Example 2:
+Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+Output: 10
+Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+Constraints:
+1 <= nums.length <= 10^5
+nums[i] is either 0 or 1.
+0 <= k <= nums.length
+
+Link: https://leetcode.com/explore/challenge/card/june-leetcoding-challenge-2021/607/week-5-june-29th-june-30th/3796/ */
+
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int start = 0;
+        int end = 0;
+        while(end<nums.length){
+            if(nums[end]==0){
+                k--;
+            }
+            if(k<0){
+                if(nums[start]==0){
+                    k++;
+                }
+                start++;
+            }
+            end++;
+        }
+        return end-start;
+    }
+}
