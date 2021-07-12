8e905b07ad38e536cc648fd5dd37db61760234ab
ela4490 <ela4490@gmail.com>
2021-07-12 13:25:50 +0200

    Create 205. Isomorphic Strings

---
+++ Leet Code/Easy/String/205. Isomorphic Strings/Solution.java
@@ -0,0 +1,42 @@
+/* Given two strings s and t, determine if they are isomorphic. Two strings s and t are isomorphic if the characters in s can be replaced to get t. All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+Example 1:
+Input: s = "egg", t = "add"
+Output: true
+
+Example 2:
+Input: s = "foo", t = "bar"
+Output: false
+
+Example 3:
+Input: s = "paper", t = "title"
+Output: true
+
+Constraints:
+1 <= s.length <= 5 * 104
+t.length == s.length
+s and t consist of any valid ascii character.
+
+Link: https://leetcode.com/problems/isomorphic-strings/ */
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+        
+        if(s==null || t==null || s.length()!=t.length()){
+            return false;
+        }
+        
+        for(int i=0; i<s.length(); i++){
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if(m1[c1]!=m2[c2]){
+                return false;
+            }
+            m1[c1] = i+1;
+            m2[c2] = i+1;
+        }
+        return true;
+    }
+}
