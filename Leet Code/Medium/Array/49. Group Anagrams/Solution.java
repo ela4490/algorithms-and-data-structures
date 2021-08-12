cbb3399c2bfb57e79f9cbc3526e821932d5a2e15
ela4490 <ela4490@gmail.com>
2021-08-12 14:48:28 +0200

    Create 49. Group Anagrams

---
+++ Leet Code/Medium/Array/49. Group Anagrams/Solution.java
@@ -0,0 +1,40 @@
+/* Given an array of strings strs, group the anagrams together. You can return the answer in any order. An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+
+Constraints:
+1 <= strs.length <= 10^4
+0 <= strs[i].length <= 100
+strs[i] consists of lower-case English letters.
+
+Link: https://leetcode.com/problems/group-anagrams/ */
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            String key = buildKey(s);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(s);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+    private String buildKey(String s){
+        char[] keyChars = new char[26];
+        for(char c : s.toCharArray()){
+            keyChars[c-'a']++;
+        }
+        return new String(keyChars);
+    }
+}
