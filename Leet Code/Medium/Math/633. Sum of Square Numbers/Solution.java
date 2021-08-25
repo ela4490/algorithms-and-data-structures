101ab6c13d659d0a8a843ab34513a3aff3b3edec
ela4490 <ela4490@gmail.com>
2021-08-25 20:07:22 +0200

    633. Sum of Square Numbers

---
+++ Leet Code/Medium/Math/633. Sum of Square Numbers/Solution.java
@@ -0,0 +1,47 @@
+/* Given a non-negative integer c, decide whether there're two integers a and b such that a^2 + b^2 = c.
+
+Example 1:
+Input: c = 5
+Output: true
+Explanation: 1 * 1 + 2 * 2 = 5
+
+Example 2:
+Input: c = 3
+Output: false
+
+Example 3:
+Input: c = 4
+Output: true
+
+Example 4:
+Input: c = 2
+Output: true
+
+Example 5:
+Input: c = 1
+Output: true
+
+Constraints:
+0 <= c <= 2^31 - 1
+
+Link: https://leetcode.com/problems/sum-of-square-numbers/ */
+
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        int left = 0;
+        int right = (int)Math.sqrt(c);
+        
+        while(left<=right) {
+            int total = left*left + right*right;
+            if(total==c) {
+                return true;
+            } else if(total<c) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return false;
+    }
+}
