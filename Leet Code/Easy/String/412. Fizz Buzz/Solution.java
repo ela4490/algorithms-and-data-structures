02edb1e50df6ab46eb062187ee271fbd5a644ce1
ela4490 <ela4490@gmail.com>
2021-09-06 19:03:02 +0200

    Create 412. Fizz Buzz

---
+++ Leet Code/Easy/String/412. Fizz Buzz/Solution.java
@@ -0,0 +1,46 @@
+/* Given an integer n, return a string array answer (1-indexed) where:
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i if non of the above conditions are true.
+
+Example 1:
+Input: n = 3
+Output: ["1","2","Fizz"]
+
+Example 2:
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+
+Example 3:
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+Constraints:
+1 <= n <= 10^4
+    
+Link: https://leetcode.com/problems/fizz-buzz/ */
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        
+        List<String> list = new ArrayList<String>();
+        
+        for(int i = 1; i <= n; i++) {
+            
+            if(i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if(i % 3 == 0) {
+                list.add("Fizz");
+            } else if(i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(String.valueOf(i));
+            }
+            
+        }
+        
+        return list;
+        
+    }
+}
