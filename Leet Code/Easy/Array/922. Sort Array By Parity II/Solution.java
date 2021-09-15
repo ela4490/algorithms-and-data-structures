d3021ac7d88e5c8a330302d01ea780df0d94bbd9
ela4490 <ela4490@gmail.com>
2021-09-15 21:58:05 +0200

    Create 922. Sort Array By Parity II

---
+++ Leet Code/Easy/Array/922. Sort Array By Parity II/Solution.java
@@ -0,0 +1,39 @@
+/* Given an array of integers nums, half of the integers in nums are odd, and the other half are even. Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even. Return any answer array that satisfies this condition.
+
+Example 1:
+Input: nums = [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+
+Example 2:
+Input: nums = [2,3]
+Output: [2,3]
+
+Constraints:
+2 <= nums.length <= 2 * 10^4
+nums.length is even.
+Half of the integers in nums are even.
+0 <= nums[i] <= 1000
+
+Follow Up: Could you solve it in-place?
+    
+Link: https://leetcode.com/problems/sort-array-by-parity-ii/ */
+
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int n = nums.length;
+        int even = 0;
+        int odd = 1;
+        
+        while(true) {
+            while(even < n && nums[even] % 2 == 0) even += 2;
+            while(odd < n && nums[odd] % 2 != 0) odd += 2;
+            
+            if(even >= nums.length && odd >= nums.length) return nums;
+            
+            int temp = nums[even];
+            nums[even] = nums[odd];
+            nums[odd] = temp;
+        }
+    }
+}
