4ee9a8ed5b2ae51df8565126fadf5754d1ee29be
ela4490 <ela4490@gmail.com>
2021-09-23 21:02:03 +0200

    Create 1328. Break a Palindrome

---
+++ Leet Code/Coding Challenges/September LeetCode Challenge 2021/Week 4 September 22nd - September 28th/1328. Break a Palindrome/Solution.java
@@ -0,0 +1,47 @@
+/* Given a palindromic string of lowercase English letters palindrome, replace exactly one character with any lowercase English letter so that the resulting string is not a palindrome and that it is the lexicographically smallest one possible. Return the resulting string. If there is no way to replace a character to make it not a palindrome, return an empty string. A string a is lexicographically smaller than a string b (of the same length) if in the first position where a and b differ, a has a character strictly smaller than the corresponding character in b. For example, "abcc" is lexicographically smaller than "abcd" because the first position they differ is at the fourth character, and 'c' is smaller than 'd'.
+
+Example 1:
+Input: palindrome = "abccba"
+Output: "aaccba"
+Explanation: There are many ways to make "abccba" not a palindrome, such as "zbccba", "aaccba", and "abacba".
+Of all the ways, "aaccba" is the lexicographically smallest.
+
+Example 2:
+Input: palindrome = "a"
+Output: ""
+Explanation: There is no way to replace a single character to make "a" not a palindrome, so return an empty string.
+
+Example 3:
+Input: palindrome = "aa"
+Output: "ab"
+
+Example 4:
+Input: palindrome = "aba"
+Output: "abb"
+
+Constraints:
+1 <= palindrome.length <= 1000
+palindrome consists of only lowercase English letters.
+    
+Link: https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/639/week-4-september-22nd-september-28th/3985/ */
+
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        char[] charArray = palindrome.toCharArray();
+        
+        if(charArray.length < 2) {
+            return "";
+        }
+        
+        for(int i = 0; i < charArray.length/2; i++) {
+            if(charArray[i] != 'a') {
+                charArray[i] = 'a';
+                return String.valueOf(charArray);
+            }
+        }
+        
+        charArray[charArray.length - 1] = 'b';
+        
+        return String.valueOf(charArray);
+    }
+}
