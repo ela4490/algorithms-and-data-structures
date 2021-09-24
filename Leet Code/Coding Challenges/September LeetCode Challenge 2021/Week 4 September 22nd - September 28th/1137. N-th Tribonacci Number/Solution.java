511f778bc9d13b23647aa70ec77bfa36909296cf
ela4490 <ela4490@gmail.com>
2021-09-24 11:04:07 +0200

    Create 1137. N-th Tribonacci Number

---
+++ Leet Code/Coding Challenges/September LeetCode Challenge 2021/Week 4 September 22nd - September 28th/1137. N-th Tribonacci Number/Solution.java
@@ -0,0 +1,46 @@
+/* The Tribonacci sequence Tn is defined as follows: 
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+Given n, return the value of Tn.
+
+Example 1:
+Input: n = 4
+Output: 4
+Explanation:
+T_3 = 0 + 1 + 1 = 2
+T_4 = 1 + 1 + 2 = 4
+
+Example 2:
+Input: n = 25
+Output: 1389537
+
+Constraints:
+0 <= n <= 37
+The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
+
+Link: https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/639/week-4-september-22nd-september-28th/3986/ */
+
+class Solution {
+    public int tribonacci(int n) {
+        if(n == 0) {
+            return 0;
+        } else if(n <= 2) {
+            return 1;
+        } else {
+            int a = 0;
+            int b = 1;
+            int c = 1;
+            int sum = 0;
+            int index = 3;
+            
+            while(index <= n) {
+                sum = a + b + c;
+                a = b;
+                b = c;
+                c = sum;
+                index++;
+            }
+            
+            return sum;
+        }
+    }
+}
