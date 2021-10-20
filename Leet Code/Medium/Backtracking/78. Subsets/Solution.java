71a5a80cb1f18c6461ff253dbd1bdca5fe05c7f7
ela4490 <ela4490@gmail.com>
2021-10-20 16:09:15 +0200

    Create 78. Subsets

---
+++ Leet Code/Medium/Backtracking/78. Subsets/Solution.java
@@ -0,0 +1,35 @@
+/* Given an integer array nums of unique elements, return all possible subsets (the power set). The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+Example 2:
+Input: nums = [0]
+Output: [[],[0]]
+
+Constraints:
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+
+Link: https://leetcode.com/problems/subsets/ */
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<Integer>(), result);
+        
+        return result;
+    }
+    
+    private void backtrack(int[] nums, int start, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));
+        
+        for(int i = start; i < nums.length; i++) {
+            current.add(nums[i]);
+            backtrack(nums, i + 1, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+}
