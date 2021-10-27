62b7a854bcc099cc048244b1a7d0891c34435304
ela4490 <ela4490@gmail.com>
2021-10-27 16:15:33 +0200

    Update 75. Sort Colors with One Pass Solution

--- Leet Code/Medium/Array/75. Sort Colors/Solution.java
+++ Leet Code/Medium/Array/75. Sort Colors/Solution.java
@@ -23,9 +23,37 @@ nums[i] is 0, 1, or 2.
 
 Follow up: Could you come up with a one-pass algorithm using only constant extra space?
 
-    
 Link: https://leetcode.com/problems/sort-colors/ */
 
+//One Pass
+class Solution {
+    public void sortColors(int[] nums) {
+        int zeroIndex = 0;
+        int twoIndex = nums.length - 1;
+        
+        for(int i = zeroIndex; i <= twoIndex;) {
+            
+            if(nums[i] == 0) {
+                swap(nums, zeroIndex, i);
+                i++;
+                zeroIndex++;
+            } else if(nums[i] == 2) {
+                swap(nums, twoIndex, i);
+                twoIndex--;
+            } else {
+                i++;
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int start, int end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+    }
+}
+
+//Two Pass
 class Solution {
     public void sortColors(int[] nums) {
         int zero = 0;
