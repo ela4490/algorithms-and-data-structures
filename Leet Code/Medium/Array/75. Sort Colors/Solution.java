0d83cf4365092729d72536f6142929643c4c8fc2
ela4490 <ela4490@gmail.com>
2021-10-27 12:07:33 +0200

    Create 75. Sort Colors

---
+++ Leet Code/Medium/Array/75. Sort Colors/Solution.java
@@ -0,0 +1,63 @@
+/* Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue. We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively. You must solve this problem without using the library's sort function.
+
+Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Example 3:
+Input: nums = [0]
+Output: [0]
+
+Example 4:
+Input: nums = [1]
+Output: [1]
+
+Constraints:
+n == nums.length
+1 <= n <= 300
+nums[i] is 0, 1, or 2.
+
+Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+
+    
+Link: https://leetcode.com/problems/sort-colors/ */
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+        
+        for(int i = 0; i < nums.length; i++) {
+            
+            if(nums[i] == 0) zero++;
+            else if(nums[i] == 1) one++;
+            else two++;
+        
+        }
+        
+        int index = 0;
+        while(zero > 0) {
+            nums[index] = 0;
+            zero--;
+            index++;
+        }
+            
+        while(one > 0) {
+            nums[index] = 1;
+            one--;
+            index++;
+        }
+            
+        while(two > 0) {
+            nums[index] = 2;
+            two--;
+            index++;
+        }
+        
+    }
+}
