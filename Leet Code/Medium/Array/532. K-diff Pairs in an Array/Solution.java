27b40a870a7015bc10f751bf05060a985406809c
ela4490 <ela4490@gmail.com>
2022-02-09 18:22:13 +0100

    Create 532. K-diff Pairs in an Array

---
+++ Leet Code/Medium/Array/532. K-diff Pairs in an Array/Solution.java
@@ -0,0 +1,57 @@
+/* Given an array of integers nums and an integer k, return the number of unique k-diff pairs in the array. A k-diff pair is an integer pair (nums[i], nums[j]), where the following are true:
+0 <= i < j < nums.length
+|nums[i] - nums[j]| == k
+
+Notice that |val| denotes the absolute value of val.
+
+Example 1:
+Input: nums = [3,1,4,1,5], k = 2
+Output: 2
+Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+Although we have two 1s in the input, we should only return the number of unique pairs.
+
+Example 2:
+Input: nums = [1,2,3,4,5], k = 1
+Output: 4
+Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4) and (4, 5).
+
+Example 3:
+Input: nums = [1,3,1,5,4], k = 0
+Output: 1
+Explanation: There is one 0-diff pair in the array, (1, 1).
+
+Constraints:
+1 <= nums.length <= 10^4
+-10^7 <= nums[i] <= 10^7
+0 <= k <= 10^7
+    
+Link: https://leetcode.com/problems/k-diff-pairs-in-an-array/ */
+
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        if(nums.length == 0) {
+            return 0;
+        }
+        
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(k == 0) {
+                if(entry.getValue() >= 2) {
+                    count++;
+                }
+            } else {
+                if(map.containsKey(entry.getKey() + k)) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
