9e2760bcdc43c0b54f095f2034967938828c4473
ela4490 <ela4490@gmail.com>
2022-02-21 18:35:05 +0100

    Update 169. Majority Element

--- Leet Code/Easy/Array/169. Majority Element/Solution.java
+++ Leet Code/Easy/Array/169. Majority Element/Solution.java
@@ -15,9 +15,32 @@ n == nums.length
     
 Link: https://leetcode.com/problems/majority-element/ */
 
+// Time Complexity : O(NlogN)
 class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length/2];
+    }
+}
+
+// Time Complexity : O(N)
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majority = nums[0];
+        int count = 1;
+        
+        for(int i = 1; i < nums.length; i++) {
+            
+            if(majority == nums[i]) count++;
+            else count--;
+            
+            if(count == 0) {
+                majority = nums[i];
+                count = 1;
+            }
+            
+        }
+        
+        return majority;
     }
 }
