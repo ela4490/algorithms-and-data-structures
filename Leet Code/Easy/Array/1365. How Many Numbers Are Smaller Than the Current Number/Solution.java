f68080d1f2bf2494d9dc4f20f318e29e8a25feb0
ela4490 <ela4490@gmail.com>
2022-02-22 19:14:18 +0100

    Update 1365. How Many Numbers Are Smaller Than the Current Number

--- Leet Code/Easy/Array/1365. How Many Numbers Are Smaller Than the Current Number/Solution.java
+++ Leet Code/Easy/Array/1365. How Many Numbers Are Smaller Than the Current Number/Solution.java
@@ -24,18 +24,42 @@ Constraints:
     
 Link: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/ */
 
+// Time Complexity : O(N^2)
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int[] result = new int[nums.length];
-        for(int i=0; i<nums.length; i++){
+        
+	for(int i = 0; i < nums.length; i++){
             int count = 0;
-            for(int j = 0; j<nums.length; j++){
-                if(nums[i]>nums[j]){
+            for(int j = 0; j < nums.length; j++){
+                if(nums[i] > nums[j]){
                     count++;
                 }
             }
             result[i] = count;
         }
+
         return result;
+    }
+}
+
+// Time Complexity : O(N)
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] count = new int[101];
+        
+        for(int i = 0; i < nums.length; i++) {
+            count[nums[i]]++;
+        }
+        
+        for(int i = 1; i < count.length; i++) {
+            count[i] += count[i - 1];
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = (nums[i]  == 0) ? 0 : count[nums[i] - 1];
+        }
+        
+        return nums;
     }
 }
