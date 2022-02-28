2f2aedeea9e715bfc08a1fd034d6b3ddc8ba626a
ela4490 <ela4490@gmail.com>
2022-02-28 19:44:06 +0100

    Create 1290. Convert Binary Number in a Linked List to Integer

---
+++ Leet Code/Easy/Linked List/1290. Convert Binary Number in a Linked List to Integer/Solution.java
@@ -0,0 +1,41 @@
+/* Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number. Return the decimal value of the number in the linked list.
+
+Example 1:
+Input: head = [1,0,1]
+Output: 5
+Explanation: (101) in base 2 = (5) in base 10
+
+Example 2:
+Input: head = [0]
+Output: 0
+
+Constraints:
+The Linked List is not empty.
+Number of nodes will not exceed 30.
+Each node's value is either 0 or 1.
+    
+Link: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        ListNode current = head;
+        int num = 0;
+        
+        while(current != null) {
+            num = 2 * num + current.val;
+            current = current.next;
+        }
+        
+        return num;
+    }
+}
