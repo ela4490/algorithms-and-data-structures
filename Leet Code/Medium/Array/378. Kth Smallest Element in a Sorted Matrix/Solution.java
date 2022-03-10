872c61f7c9291896fffd5e0d9fe5fc1ff9874bb9
ela4490 <ela4490@gmail.com>
2022-03-10 18:33:34 +0100

    Update 378. Kth Smallest Element in a Sorted Matrix

--- Leet Code/Medium/Array/378. Kth Smallest Element in a Sorted Matrix/Solution.java
+++ Leet Code/Medium/Array/378. Kth Smallest Element in a Sorted Matrix/Solution.java
@@ -19,14 +19,17 @@ All the rows and columns of matrix are guaranteed to be sorted in non-decreasing
 
 Link: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/ */
 
+Time Complexity : O(N^2)
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         List<Integer> list = new ArrayList<>();
-        for(int i=0; i<matrix.length; i++){
+        
+	for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[i].length; j++){
                 list.add(matrix[i][j]);
             }
         }
+
         Collections.sort(list);
         return list.get(k-1);
     }
