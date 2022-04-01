2b174bf0a61054a94398c56512aa0d0b39a48ddf
Ela Singh <30351771+ela4490@users.noreply.github.com>
2022-04-01 12:30:50 +0200

    Update  101. Symmetric Tree

--- Leet Code/Easy/Tree/101. Symmetric Tree/Solution.java
+++ Leet Code/Easy/Tree/101. Symmetric Tree/Solution.java
@@ -16,17 +16,30 @@ Follow up: Could you solve it both recursively and iteratively?
     
 Link: https://leetcode.com/problems/symmetric-tree/ */
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] values = new int[2];
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i+1; j < nums.length; j++) {
-                if (target == (nums[i] + nums[j])) {
-                    values[0] = i;
-                    values[1] = j;
-                }
-            }
-        }
-        return values;
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        return isSymmetric(root.left, root.right);
+    }
+    public boolean isSymmetric(TreeNode left, TreeNode right){
+        if(left==null && right==null) return true;
+        if(left==null || right==null) return false;
+        if(left.val!=right.val) return false;
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }
 }
